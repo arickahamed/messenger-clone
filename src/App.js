68f38c4030453cef1f7ba0622fa7c2cbabec8e6b
arickahamed <arickahamed700@gmail.com>
2020-07-17 14:11:15 +0600

    after importing mui, made message dynamic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
+import "./App.css";
+import Message from "./Message";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState(["a", "b", "c"]);
+  useEffect(() => {
+    console.log(input);
+    console.log(messages);
+  });
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    //all the logic to send a message here.
+    setMessages([...messages, input]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world!</h1>
+
+      <form>
+        <FormControl>
+          <InputLabel>Enter message</InputLabel>
+          <Input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <Button
+            disabled={!input}
+            variant="contained"
+            color="primary"
+            type="submit"
+            onClick={sendMessage}
+          >
+            Send
+          </Button>
+        </FormControl>
+      </form>
+
+      {messages.map((msg) => (
+        <Message text={msg}></Message>
+      ))}
     </div>
   );
 }
